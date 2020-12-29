b4e195040b0e425de81be9ce07cf73b2ecf89771
Tomasz Kordiak <dotomika@gmail.com>
2020-12-29 15:54:35 +0100

    Fix detect negative numbers

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -9,7 +9,8 @@ def make_correct(string_input: str):
     - Remove spaces on first and last positions.
     - Change commas on dots.
     - Remove equal's mark on end.
-    - Add multiplication's marks before parentheses and roots."""
+    - Add multiplication's marks before parentheses and roots.
+    - Rewrite spaces."""
 
     marks = '(^√∛∜*/+- '
     positions = list()
@@ -48,6 +49,18 @@ def make_correct(string_input: str):
     if positions:
         change = True
         log.debug(f"Add multiplication's marks in positions: {positions}")
+
+    # Rewrite spaces
+    string_input = string_input.replace(' ', '')
+    marks_positions = [pos for pos in range(len(string_input)) if string_input[pos] in '+-' and string_input[pos - 1] not in marks[:-1] and pos > 0]
+    marks_positions.reverse()
+
+    for pos in marks_positions:
+        string_input = string_input[:pos] + ' ' + string_input[pos] + ' ' + string_input[pos + 1:]
+
+    string_input = string_input.replace('  ', ' ')
+    string_input = string_input.replace('- - ', '- -')
+            
 
     # Add info to log
     if change:
@@ -92,3 +105,7 @@ def calculate(string_input: str, round_to=None):
 
     log.debug(f"Final result: {list_input[0]}\n")
     return list_input[0], string_calculation
+
+if __name__ == "__main__":
+    string_input = '- 2 - 3'
+    print(make_correct(string_input))
